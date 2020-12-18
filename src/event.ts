6b497527885e3af540c2340cc6a929827701bd3b
Ze-Hao Wang <salmon.zh.tw@gmail.com>
2020-12-18 22:44:51 +0800

    support events API

--- src/event.ts
+++ src/event.ts
@@ -3,11 +3,11 @@ import _ from 'lodash';
 import fetch from 'node-fetch';
 import { parse } from 'node-html-parser';
 import urlJoin from 'url-join';
+import moment from 'moment';
 import type { HTMLElement } from 'node-html-parser';
 
 const hostUrl = 'https://leekduck.com';
 
-// TODO:
 const getEvents = async () => {
   const url = urlJoin(hostUrl, '/events/');
   const res = await fetch(url);
@@ -17,26 +17,33 @@ const getEvents = async () => {
   const currentEventItems = root.querySelectorAll('div.events-list.current-events a.event-item-link');
   const upcomingEventItems = root.querySelectorAll('div.events-list.upcoming-events a.event-item-link');
 
-  const formatEvent = (eventItem: HTMLElement) => {
+  const formatEvent = (eventItem: HTMLElement, label: string) => {
     const title = eventItem.querySelector('h2').rawText;
     const imageUrl = urlJoin(
       hostUrl,
       eventItem.querySelector('.event-img-wrapper img').getAttribute('src')!,
     );
-    const startTime = eventItem.querySelector('.event-countdown').getAttribute('data-countdown');
-    const endTime = eventItem.querySelector('.event-countdown').getAttribute('data-countdown');
+    const countdownNode = eventItem.querySelector('.event-countdown');
+    const countdownTo = countdownNode.getAttribute('data-countdown-to')!;
+    const timeRaw = countdownNode.getAttribute('data-countdown')!;
+    const isLocaleTime = ['start', 'end'].includes(countdownTo) ? !/^\d+$/.test(timeRaw) : null;
+    const time = isLocaleTime ? moment(timeRaw).toISOString() : moment.unix(parseInt(timeRaw) / 1000).toISOString();
+    const startTime = countdownTo === 'start' ? time : null;
+    const endTime = countdownTo === 'end' ? time : null;
 
     return {
       title,
       imageUrl,
+      label,
+      isLocaleTime,
       startTime,
       endTime,
     };
   };
 
   const events = [
-    ...currentEventItems.map(formatEvent),
-    ...upcomingEventItems.map(formatEvent),
+    ...currentEventItems.map((item) => formatEvent(item, 'current')),
+    ...upcomingEventItems.map((item) => formatEvent(item, 'upcoming')),
   ];
 
   return events;
